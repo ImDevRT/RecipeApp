6b3a4c7256b6cc4902704617dc5b98732e94fb72
Dev Thakkar <imdevrt@gmail.com>
2018-08-26 14:47:46 +0530

    added backend for fetch/save recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
+
+}
