9a6c21ba8cf6ea223ee92487952d8ef8c2a2f31f
Dev Thakkar <imdevrt@gmail.com>
2018-08-31 12:10:26 +0530

    Added SignIn/Up and Lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router"
-import { AppComponent } from "./app.component";
-import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { HomeComponent } from "./home/home.component";
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', component: RecipesComponent, children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipeDetailComponent },
-        { path: ':id/edit', component: RecipeEditComponent }
-    ]},
-    { path: 'shopping-list', component: ShoppingListComponent}
-]
+    { path: '', component: HomeComponent},
+    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
+    { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule' }
+];
 
 @NgModule({
     imports: [
         RouterModule.forRoot(appRoutes)
     ],
-    exports: [RouterModule]
+    exports: [ RouterModule ]
 })
 
 export class AppRoutingModule {}
