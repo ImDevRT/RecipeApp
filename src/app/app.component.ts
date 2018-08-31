7f0dec8c7692e061488381d87329be96a93d00c6
Dev Thakkar <30868095+ImDevRT@users.noreply.github.com>
2018-08-31 16:11:15 +0530

    added app component with DB init

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "Your-API-Key",
+      authDomain: "Your..."
+    });
+  }
+
+}
