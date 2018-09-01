e26b5cbb42a734b28a2b60c4d915bc9b74f38d54
Dev Thakkar <imdevrt@gmail.com>
2018-09-01 14:50:54 +0530

    added modules preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router"
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router"
 import { HomeComponent } from "./core/home/home.component";
 
 const appRoutes: Routes = [
@@ -10,7 +10,7 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
     ],
     exports: [RouterModule]
 })
